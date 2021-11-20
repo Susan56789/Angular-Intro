6cf875a78281d7c33ee119c2879e35e146a4a575
Susan Wairimu <mwihaki2susan@gmail.com>
2021-11-20 15:16:22 +0300

    fetching tasks from json-server

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -2,16 +2,17 @@ import { Injectable } from '@angular/core';
 import { TASKS } from 'src/app/mock-tasks';
 import { Task } from 'src/app/Task';
 import { Observable, of } from 'rxjs';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
+private apiUrl = 'http://localhost:5000/tasks';
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
 
   getTasks(): Observable<Task[]> {
-     const tasks = of(TASKS);
-     return tasks;
+     return this.http.get<Task[]>(this.apiUrl)
   }
 }
