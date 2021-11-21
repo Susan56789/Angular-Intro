150678473e5aa68dfc76e1fabc1175849918f92b
Susan Wairimu <mwihaki2susan@gmail.com>
2021-11-21 19:13:49 +0300

    limit add task button to task page only

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UiService } from 'src/app/services/ui.service';
 import { Subscription } from 'rxjs';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -13,7 +14,7 @@ export class HeaderComponent implements OnInit {
 showAddTask: boolean;
 subscription: Subscription;
 
-  constructor(private uiService:UiService) {
+  constructor(private uiService:UiService, private router:Router) {
 this.subscription = this.uiService.onToggle().subscribe(value => this.showAddTask = value);
    }
 
@@ -22,5 +23,9 @@ this.subscription = this.uiService.onToggle().subscribe(value => this.showAddTas
 
   toggleAddTask(){
     this.uiService.toggleAddTask();
+  }
+
+  hasRoute(route: string){
+    return this.router.url === route;
   }
 }
